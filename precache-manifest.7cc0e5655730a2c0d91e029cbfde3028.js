d157fd75e55031767364001091e89912265b02e4
a13x5u5 <al3xsus@pm.me>
2020-04-12 17:42:40 +0600

    Updates

---
+++ precache-manifest.7cc0e5655730a2c0d91e029cbfde3028.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "57f248690f7f95a1cbdc",
+    "url": "/react-4-out-of-20/static/css/main.b5a743ba.chunk.css"
+  },
+  {
+    "revision": "57f248690f7f95a1cbdc",
+    "url": "/react-4-out-of-20/static/js/main.57f24869.chunk.js"
+  },
+  {
+    "revision": "0f26932f03f171f85ef9",
+    "url": "/react-4-out-of-20/static/js/1.0f26932f.chunk.js"
+  },
+  {
+    "revision": "f65490c7d746ec599079",
+    "url": "/react-4-out-of-20/static/js/runtime~main.f65490c7.js"
+  },
+  {
+    "revision": "479f028922ba997857a755eaee7eb7b2",
+    "url": "/react-4-out-of-20/index.html"
+  }
+];
