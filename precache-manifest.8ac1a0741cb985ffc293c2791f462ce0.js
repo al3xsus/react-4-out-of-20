8e55b075dc906acddb63cce1cfe65ef6cdeca91f
a13x5u5 <al3xsus@pm.me>
2020-04-12 17:53:44 +0600

    Updates

---
+++ precache-manifest.8ac1a0741cb985ffc293c2791f462ce0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877dac22a436e4f8f36c65da0c0da48b",
+    "url": "/react-4-out-of-20/index.html"
+  },
+  {
+    "revision": "ecfbda941d2663e47d18",
+    "url": "/react-4-out-of-20/static/css/main.ec1790b9.chunk.css"
+  },
+  {
+    "revision": "eaff4f1844112b4f9bef",
+    "url": "/react-4-out-of-20/static/js/2.015b925e.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/react-4-out-of-20/static/js/2.015b925e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecfbda941d2663e47d18",
+    "url": "/react-4-out-of-20/static/js/main.ceccbde7.chunk.js"
+  },
+  {
+    "revision": "ef27ff82a9cbbbf9aacd",
+    "url": "/react-4-out-of-20/static/js/runtime-main.49400a81.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-4-out-of-20/precache-manifest.7cc0e5655730a2c0d91e029cbfde3028.js"
+  "/react-4-out-of-20/precache-manifest.8ac1a0741cb985ffc293c2791f462ce0.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-4-out-of-20/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-4-out-of-20/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
